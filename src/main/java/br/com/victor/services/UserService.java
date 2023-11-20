b6a5800d41350e86553c2d6012c6cd0990fa29a8
victorsousalima <victorsousa16042005@gmail.com>
2023-11-20 14:22:34 -0300

    User update with PUT

--- src/main/java/br/com/victor/services/UserService.java
+++ src/main/java/br/com/victor/services/UserService.java
@@ -36,7 +36,24 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+
+        return userRepository.save(newUser);
+    }
+
     public User fromDTO(UserDTO userDTO) {
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
+    private void updateData(User newUser, User user) {
+        if (user.getName() != null) {
+            newUser.setName(user.getName());
+        }
+
+        if (user.getEmail() != null) {
+            newUser.setEmail(user.getEmail());
+        }
     }
 }
