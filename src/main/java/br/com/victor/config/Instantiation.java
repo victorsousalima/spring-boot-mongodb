bbc5804da227e4389c0d17c2ad77fd7a4bf17690
victorsousalima <victorsousa16042005@gmail.com>
2023-11-20 15:05:00 -0300

    Referencing user posts

--- src/main/java/br/com/victor/config/Instantiation.java
+++ src/main/java/br/com/victor/config/Instantiation.java
@@ -41,6 +41,10 @@ public class Instantiation implements CommandLineRunner{
         Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+
+        userRepository.save(maria);
     }
     
 }
